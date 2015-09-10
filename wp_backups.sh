4763ce008fd0318c74ef4cad82ff83fbf5e3ee3b
zebus3d <zebus3dream@gmail.com>
2015-09-10 15:18:39 +0200

    Update wp_backups.sh
    licenciando

--- wp_backups.sh
+++ wp_backups.sh
@@ -6,6 +6,19 @@
 # For security i recommend:
 # chmod 700 wp_backup.sh
 #####################################################################################
+'''
+Copyright (c) 2012 Jorge Hernandez - Melendez
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+GNU General Public License for more details.
+You should have received a copy of the GNU General Public License
+along with this program. If not, see <http://www.gnu.org/licenses/>.
+'''
 
 # Chek Root User:
 if [ "$(id -u)" != "0" ]; then
