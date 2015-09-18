93659e694cb0c077837ff1d28b85e952642eb70c
zebus3d <zebus3dream@gmail.com>
2015-09-18 16:24:36 +0200

    Create regex.py
    probando expresiones regulares con python :)

---
+++ regex.py
@@ -0,0 +1,12 @@
+import re
+
+contenido="opcion = 1400"
+
+resultado = re.search('(^opcion)\s=\s(.*)', contenido)
+
+print("\ngrupo 0 (todo el contenido):")
+print(resultado.group(0))
+print("\ngrupo 1:")
+print(resultado.group(1))
+print("\ngrupo 2:")
+print(resultado.group(2))
